bd57a673604059e025f9ffd1325c143b490e31da
swatinp6 <swatinpatil6@gmail.com>
2024-03-27 11:04:47 +0530

    task

---
+++ javascript/color.js
@@ -0,0 +1,10 @@
+function change_Color(){
+    //alert('Tet Case one')
+    document.getElementById('mobile').style.backgroundColor ="blue"
+}
+
+function change_Case(){
+    //alert('Tet Case Two')
+    var str = document.getElementById('case').value
+document.getElementById('case').value = str.toUpperCase();
+}
