4f9d4f91fdbe4b2a0fca9879e24efb27e48b815a
swatinp6 <swatinpatil6@gmail.com>
2024-03-27 21:08:43 +0530

    iterative

---
+++ javascript/JSiterative_st/Q1.js
@@ -0,0 +1,9 @@
+//Write a program to print 1 to 10 numbers using a while loop?
+var i=1
+while(i<=10){
+    console.log(i);
+    i++
+}
+
+
+
---
+++ javascript/JSiterative_st/Q2.js
@@ -0,0 +1,6 @@
+// Write a program to print 10 to 1 number using a while loop?
+var i=10
+while(i>=1){
+    console.log(i);
+    i--
+}
