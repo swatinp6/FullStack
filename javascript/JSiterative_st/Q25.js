a3f979f49abb57cefbcd7e4862f3b879a685ea0e
swatinp6 <swatinpatil6@gmail.com>
2024-04-02 11:19:31 +0530

    iterativest

--- javascript/JSiterative_st/Q25.js
+++ javascript/JSiterative_st/Q25.js
@@ -1,20 +1,20 @@
 
 // program to check an Armstrong number of three digits
 
-//let sum = 0;
-//const number = 407
-//let temp = number;
-//while (temp > 0) {
-  //  let remainder = temp % 10;
-    //sum += remainder * remainder * remainder;
-    //temp = parseInt(temp / 10); 
-//}
-//if (sum == number) {
-  //  console.log(`${number} is an Armstrong number`);
-//}
-//else {
-  //  console.log(`${number} is not an Armstrong number.`);
-//}
+/*let sum = 0;
+const number = 407
+let temp = number;
+while (temp > 0) {
+    let remainder = temp % 10;
+    sum += remainder * remainder * remainder;
+    temp = parseInt(temp / 10); 
+}
+if (sum == number) {
+    console.log(`${number} is an Armstrong number`);
+}
+else {
+    console.log(`${number} is not an Armstrong number.`);
+}*/
 
 
 
