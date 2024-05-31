7142a4c2ec27c15a61ed7f0f1c3317d8182188d8
swatinp6 <swatinpatil6@gmail.com>
2024-05-31 21:48:18 +0530

    test

---
+++ javascript/Events/mouseevents.js
@@ -0,0 +1,13 @@
+function change_color(){
+    document.getElementById('abc').style.backgroundColor="Blue"
+    //alert("it's working")
+    
+}
+function change_yellow(){
+    document.getElementById('xyz').style.backgroundColor="yellow"
+
+}
+function change_pink(){
+    document.getElementById('click').style.backgroundColor="pink"
+
+}
